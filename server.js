5915f59d9665a05ac9fa89a966e473a68928f712
Flavio Gouvea <flaviogouveaneto@gmail.com>
2020-10-22 19:25:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,17 @@ res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
+
+const fs = require('fs');
+const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+
+fs.readFile(f, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true}');
+
+    fs.writeFile(f, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+});
